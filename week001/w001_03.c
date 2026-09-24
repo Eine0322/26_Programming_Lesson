7ae722fc59ad89fcf67366605b70111d9e77e40a
SEOHADA <seohashin05@gmail.com>
2026-09-24 18:42:59 +0900

    week001 : 문제 3번

---
+++ week001/w001_03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a;
+    int b;
+
+    printf("가로 : ");
+    scanf("%d",&a);
+    
+    printf("세로 : ");
+    scanf("%d",&b);
+
+    printf("넓이 : %d", a*b);
+
+}
