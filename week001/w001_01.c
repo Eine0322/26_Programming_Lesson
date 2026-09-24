097880ef3e539e6ed00c699d0764512c6157b957
서하 <seohashin@seohaui-MacBookPro.local>
2026-09-24 18:34:12 +0900

    w001 : 문제 1번

---
+++ week001/w001_01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a ;
+    int b ;
+    int result ;
+
+    printf("첫 번째 숫자 : ") ;
+    scanf("%d",&a) ;
+    printf("두 번째 숫자 : ") ;
+    scanf("%d",&b);
+    
+    result=a+b ;
+    printf("%d",result) ;
+
+}
